e9bf7ddb5b258a07a993dea43207f0a25abc641f
ibsa sekata <ibsaaseek@gmail.com>
2026-04-07 15:39:27 -0300

    fix config.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,18 @@ const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
 dotenv.config();
-const db=`mysql://root:qnqWBmkMjuLzOEyEXpYjSXIiwEzGbcHH@maglev.proxy.rlwy.net:33033/railway`
+
 // Create connection pool
-const pool = mysql.createPool(
-    process.env.db);
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 // Get promise-based connection
 const promisePool = pool.promise();
